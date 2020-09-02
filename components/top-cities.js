9e5a78d022acc0c442f9783588e3fe09ac949fc1
Duncan Meech <duncan@Duncans-MBP.attlocal.net>
2020-09-02 06:03:09 -0700

    client side rendering

--- components/top-cities.js
+++ components/top-cities.js
@@ -2,7 +2,7 @@
  * store cards in a grid
  */
 
-const cities = [
+const data = [
   "New York",
   "San Francisco",
   "Los Angeles",
@@ -22,35 +22,46 @@ const cities = [
   "Austin",
 ];
 
-export default function () {
-  return (
-    <div className="container">
-      <p className="text title">Top Cities</p>
-      {cities.map((city) => (
-        <p className="text city">{city}</p>
-      ))}
-      <style jsx>{`
-        .container {
-          width: 100vw;
-          padding-top: 10px;
-          padding-bottom: 100px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          background-color: black;
-        }
-        .text {
-          color: #c4c4c4;
-          font-size: 12px;
-        }
-        .title {
-          font-family: TTNorms-Bold;
-        }
-        .city {
-          font-family: TTNorms;
-          margin: 0 0 10px 0;
-        }
-      `}</style>
-    </div>
-  );
+export default class TopCities extends React.Component {
+  state = {
+    cities: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ cities: data });
+    }, 1000);
+  }
+  render() {
+    const { cities } = this.state;
+    return (
+      <div className="container">
+        <p className="text title">Top Cities</p>
+        {cities.map((city) => (
+          <p className="text city">{city}</p>
+        ))}
+        <style jsx>{`
+          .container {
+            width: 100vw;
+            padding-top: 10px;
+            padding-bottom: 100px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background-color: black;
+          }
+          .text {
+            color: #c4c4c4;
+            font-size: 12px;
+          }
+          .title {
+            font-family: TTNorms-Bold;
+          }
+          .city {
+            font-family: TTNorms;
+            margin: 0 0 10px 0;
+          }
+        `}</style>
+      </div>
+    );
+  }
 }
